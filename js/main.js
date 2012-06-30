1559ed86b981dfe6d73e28991bb02f96b4008e9e
Jonathan Warren <jono@nmecdesign.com>
2012-06-30 15:49:06 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var searchInput = $('.input');
+var searchTerm = searchInput.val();
+
+$('.output').hide();
+
+$('#input').submit( function(){
+	goGoAjax();
+	$('.title').html('Loading...');
+	return false;
+});
+
+focusInput();
+function focusInput() {
+	searchInput.focus();
+}
+
+function resetInput() {
+	searchInput.val(null);
+}
+
+// function moveInput() {
+// 	$('.results').after($('#input'));
+// }
+
+function goGoAjax() {
+	var searchTerm = searchInput.val();
+	
+	$.ajax({
+		url: 'http://www.imdbapi.com/?t=' + searchTerm,
+		dataType: 'json',
+		success:function(data){
+			callback(data);
+		},
+		error:function(error){
+			log(error);
+			$('.title').html('Error');
+		}
+	});
+}
+
+function callback(data) {
+	if (data.Response == 'False') {
+		$('.title').html('Error');
+	} else {
+		log(data);
+		$('.output').show();
+		// moveInput();
+		resetInput();
+		focusInput();
+		$('.title').html(data.Title);		
+		$('.starring').html(data.Actors);
+		$('.director').html(data.Director);
+		$('.writer').html(data.Writer);
+		$('.genre').html(data.Genre);
+		$('.plot').html(data.Plot);
+		$('.length').html(data.Runtime);
+		$('.rated').html(data.Rated);
+		$('.released').html(data.Released);
+	}
+}
